ba8da80eb3a525af107544334ee9a97ef81bded5
matthew rendely <matthew.rendely@gmail.com>
2023-03-14 20:58:39 -0700

    done

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// code your solution here
+function saturdayFun(activity = 'roller-skate') {
+  return `This Saturday, I want to ${activity}!`;
+}
+
+const mondayWork = function (activity = 'go to the office') {
+  return `This Monday, I will ${activity}.`
+}
+
+function wrapAdjective(option) {
+  switch (option) {
+    case "*":
+      return function (adjective) { return `You are *${adjective}*!` };
+    case "||":
+      return function (adjective) { return `You are ||${adjective}||!` };
+  }
+}
+
